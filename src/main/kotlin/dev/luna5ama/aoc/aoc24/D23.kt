65ffa8432b05a0a7974dde89f1ff3d20b762bbd4
Luna <62033805+Luna5ama@users.noreply.github.com>
2025-12-02 21:05:49 -0500

    2024 D23 p2

--- src/main/kotlin/dev/luna5ama/aoc/aoc24/D23.kt
+++ src/main/kotlin/dev/luna5ama/aoc/aoc24/D23.kt
@@ -2,14 +2,13 @@ package dev.luna5ama.aoc.aoc24
 
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap
 import it.unimi.dsi.fastutil.objects.ObjectArrayList
-import it.unimi.dsi.fastutil.objects.ObjectArraySet
 import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet
 
 fun main() {
     val input = readInput("D23.txt")
     val adjList = Object2ObjectOpenHashMap<String, ObjectArrayList<String>>()
     fun insert(k: String, v: String) {
-        adjList.computeIfAbsent(k ) { ObjectArrayList()}.add(v)
+        adjList.computeIfAbsent(k) { ObjectArrayList() }.add(v)
     }
 
     input.lineSequence()
@@ -20,28 +19,57 @@ fun main() {
             insert(right, left)
         }
 
-    val sets = ObjectOpenHashSet<List<String>>()
+    val set3s = ObjectOpenHashSet<List<String>>()
 
-    adjList.forEach l1@ { (k, aNeighbors) ->
+    adjList.forEach l1@{ (k, aNeighbors) ->
         val a = k
-        aNeighbors.forEach l2@ { b ->
-            adjList[b]?.forEach l3@ { c ->
+        aNeighbors.forEach l2@{ b ->
+            adjList[b]?.forEach l3@{ c ->
                 if (c in aNeighbors) {
                     val s = mutableListOf<String>()
                     s.add(a)
                     s.add(b)
                     s.add(c)
                     s.sort()
-                    sets.add(s)
+                    set3s.add(s)
                 }
             }
         }
     }
 
-    val part1 = sets.count { set ->
+    val part1 = set3s.count { set ->
         set.any { it.startsWith('t') }
     }
 
     println("Part 1: ")
     println(part1)
+
+    var sets = ObjectOpenHashSet<ObjectOpenHashSet<String>>()
+    adjList.keys.mapTo(sets) {
+        ObjectOpenHashSet<String>().apply { add(it) }
+    }
+
+    do {
+        val newSets = ObjectOpenHashSet<ObjectOpenHashSet<String>>()
+
+        sets.forEach { oldSet ->
+            oldSet.forEach { eInOldSet ->
+                adjList[eInOldSet]!!.forEach {
+                    if (adjList[it]!!.containsAll(oldSet)) {
+                        val newSet = ObjectOpenHashSet(oldSet)
+                        if (newSet.add(it)) {
+                            newSets.add(newSet)
+                        }
+                    }
+                }
+            }
+        }
+
+        if (newSets.isNotEmpty()) {
+            sets = newSets
+        }
+    } while (newSets.isNotEmpty())
+
+    check(sets.size == 1)
+    println(sets.first().sorted().joinToString(","))
 }
